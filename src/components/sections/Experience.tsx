7a0ddc94f7a6eb5fc6bd98f59118934e8fe0bdff
Nicolás Cáceres <niosv.1104@gmail.com>
2025-11-20 07:56:24 -0300

    fix: update Experience title aling start

--- src/components/sections/Experience.tsx
+++ src/components/sections/Experience.tsx
@@ -4,8 +4,8 @@ import Experiences from '../../data/experience';
 
 export default function Exprience() {
   return (
-    <Box py={10} maxW="900px" mx="auto" id="experience">
-      <Heading mb={10} textAlign="center" size={{ base: "2xl", md: "3xl" }}>Experiencia</Heading>
+    <Box py={10} maxW="8xl" px={{ base: 5, md: 20 }} mx="auto" id="experience">
+      <Heading mb={10} textAlign="start" size={{ base: "2xl", md: "3xl" }}>Experiencia</Heading>
       <ExperienceTimeline experiences={Experiences} />
     </Box>
   )
